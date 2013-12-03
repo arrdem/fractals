b25fe1476ff6be187272dd7e0578fdfd97868432
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 22:07:57 -0600

    Refactor Fern to use the new string seq notation

--- src/main/scala/me/arrdem/garden/fern.scala
+++ src/main/scala/me/arrdem/garden/fern.scala
@@ -7,17 +7,15 @@ import scala.math.Pi
 object Fern extends Planter
 {
   def precomp(steps : Int = 5) = {
-    Transition("X" := Seq("F", "-", "[", "[", "X", "]", "+", "X", "]", "+", "F", "[", "+", "F", "X", "]", "-", "X"))
-    Transition("F" := Seq("F", "F"))
+    Transition("X" := "F-[[X]+X]+F[+FX]-X")
+    Transition("F" := "FF")
 
     State("[" := DO PUSH() DONE())
-    State("]" := DO POP() DONE())    
+    State("]" := DO POP() DONE())
     State("+" := DO TURN(-5.0*Pi/36.0) DONE())    
     State("-" := DO TURN(5.0*Pi/36.0) DONE())
     State("F" := DO DRAW(10) DONE())
 
-    var initialState = Seq("X")
-
-    compute(initialState, steps)
+    compute("X", steps)
   }
 }
