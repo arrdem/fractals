a250bad1739b097d6851bb1ecf8777f8bdda7c06
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 22:08:54 -0600

    Refactor Koch to use the string seq implicit

--- src/main/scala/me/arrdem/garden/koch.scala
+++ src/main/scala/me/arrdem/garden/koch.scala
@@ -6,14 +6,12 @@ import scala.math.Pi
 object Koch extends Planter
 {
   def precomp(steps : Int = 5) = {
-    Transition("F" := Seq("F", "+", "F", "-", "F", "-", "F", "+", "F"))
+    Transition("F" := "F+F-F-F+F")
 
     State("-" := DO TURN(Pi/2.0) DONE())
     State("+" := DO TURN(-1*Pi/2.0) DONE())
     State("F" := DO DRAW(10) DONE())
 
-    var initialState = Seq("F")
-
-    compute(initialState, steps)
+    compute("F", steps)
   }
 }
