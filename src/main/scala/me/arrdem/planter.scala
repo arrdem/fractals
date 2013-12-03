d98334c4d142570c63ae32485120ebe2332ce8ef
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 20:46:13 -0600

    Ditch some comments...

--- src/main/scala/me/arrdem/planter.scala
+++ src/main/scala/me/arrdem/planter.scala
@@ -7,10 +7,6 @@ class Planter extends _bitch
     with TurtleDSL
     with LSystem[String,Function1[Turtle, Unit]]
 {
-  /* Define a Planter object which will use both Turtle and LSystem to draw the
-   * Nth iteration of a fractal :D
-   ***************************************************************************
-   */
   def draw(k:String, t:Turtle) =
     if(_invoke_map contains(k))
       _invoke_map.get(k).getOrElse((t:Turtle) => t)(t)
@@ -19,9 +15,6 @@ class Planter extends _bitch
     for(_s <- s)
       draw(_s, t)
 
-  /* Main method, defines the L-system & draws it
-   ****************************************************************************
-   */
   def compute(initialState:Seq[String], steps:Int) = {
     val t = new Turtle
     val s = Iterator.iterate(initialState)(step).drop(steps-1).next()
