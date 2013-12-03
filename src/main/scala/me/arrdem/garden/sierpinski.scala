11c0a47860b5c0cb33c9adc4f851dd5c66b1f363
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 22:11:51 -0600

    Refactor Sierpinski to use the new string Seq implicit

--- src/main/scala/me/arrdem/garden/sierpinski.scala
+++ src/main/scala/me/arrdem/garden/sierpinski.scala
@@ -7,16 +7,14 @@ import scala.math.Pi
 object Sierpinski extends Planter
 {
   def precomp(steps : Int = 5) = {
-    Transition("A" := Seq("B", "-", "A", "-", "B"))
-    Transition("B" := Seq("A", "+", "B", "+", "A"))
+    Transition("A" := "B-A-B")
+    Transition("B" := "A+B+A")
 
     State("-" := DO TURN(Pi/3.0) DONE())
     State("+" := DO TURN(-1*Pi/3.0) DONE())
     State("A" := DO DRAW(10) DONE())
     State("B" := DO DRAW(10) DONE())
 
-    var initialState = Seq("A")
-
-    compute(initialState, steps)
+    compute("A", steps)
   }
 }
