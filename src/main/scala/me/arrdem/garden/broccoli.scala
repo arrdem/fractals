0ce9f28d0f0f8f5709eb7df6bf2a046cdcb48807
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 22:10:01 -0600

    Refactor Broccoli to use the new string Seq implicit

--- src/main/scala/me/arrdem/garden/broccoli.scala
+++ src/main/scala/me/arrdem/garden/broccoli.scala
@@ -4,20 +4,15 @@ import scala.math.Pi
 
 object Broccoli extends Planter
 {
-  /* Main method, defines the L-system & draws it
-   ****************************************************************************
-   */
   def precomp(steps : Int = 5) = {
-    Transition("0" := Seq("1", "[", "0", "]", "0"))
-    Transition("1" := Seq("1", "1"))
+    Transition("0" := "1[0]0")
+    Transition("1" := "11")
 
     State("[" := DO PUSH() TURN(Pi/4.0) DONE())
     State("]" := DO POP() TURN(-1*Pi/4.0) DONE())
     State("0" := DO DRAW(1) DONE())
     State("1" := DO DRAW(1) DONE())
 
-    var initialState = Seq("0");
-
-    compute(initialState, steps)
+    compute("0", steps)
   }
 }
