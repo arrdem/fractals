c32c38f335e97b80f89a73277b01063e753c8d6d
Reid McKenzie <rmckenzie92@gmail.com>
2013-12-02 21:06:35 -0600

    Refactor: provide reasonable failure cases & help
     - Add a help message
     - Add bounds checks on args
     - Add handling of empty args cases
     - Add handling of improper args cases

--- src/main/scala/me/arrdem/garden/runner.scala
+++ src/main/scala/me/arrdem/garden/runner.scala
@@ -1,8 +1,5 @@
-package me.arrdem.planter
-import me.arrdem.turtle._
-import me.arrdem.lsystem._
-import scala.collection.mutable.{HashMap,LinkedList}
-import scala.math.Pi
+package me.arrdem.garden
+import me.arrdem.turtle.Turtle
 
 import java.awt._
 import java.awt.geom._
@@ -10,39 +7,60 @@ import javax.swing._
 
 
 object Runner extends JFrame
-               with TurtleDSL
-               with LSystem[String,Function1[Turtle, Unit]]
 {
-  /* Main method, defines the L-system & draws it
-   ****************************************************************************
-   */
+  var halp = """Usage: run [fractal]
+    fractals: dragon     -> 15 iterations max
+              fern       -> 8  iterations max
+              kotch      -> 10 iterations max
+              broccoli   -> 12 iterations max
+              sierpinski -> 10 iterations max"""
+
   def main(args : Array[String]) = {
+
     var steps = 5
-    try {
-	 steps = augmentString(args(1)).toInt;
-    } catch {
-      case ex : ArrayIndexOutOfBoundsException => {
-        println("Iterations not specified using default of 5")
-	 } 
-	 //check for case where it was specified and was improper
-    }
-    val t : Turtle = args(0) match {
-        case "dragon" => Dragon.precomp(steps)
-        case "fern" => Fern.precomp(steps)
-        case "koch" => Koch.precomp(steps)
-        case "planter" => Planter.precomp(steps)
+    if(args.length > 0) {
+      try {
+	    steps = augmentString(args(1)).toInt;
+      } catch {
+        case ex : ArrayIndexOutOfBoundsException =>
+          println("Iterations not specified using default of 5")
+
+        case ex : NumberFormatException => {
+          println("Could not parse iteration count! aborting...")
+          sys.exit(1)
+        }
+      }
+
+      if(steps > 10)
+        println("WARNING: Running more than 10 iterations, this could be a while!")
+
+      val t : Turtle = args(0) match {
+        case "dragon"     => Dragon.precomp(steps)
+        case "fern"       => Fern.precomp(steps)
+        case "koch"       => Koch.precomp(steps)
+        case "broccoli"   => Broccoli.precomp(steps)
         case "sierpinski" => Sierpinski.precomp(steps)
-        case _ => Planter.precomp(steps)
+        case "-h" | "--help" => {
+          println(halp)
+          sys.exit(0)
+        }
+        case _            => Broccoli.precomp(steps)
       }
-    SwingUtilities.invokeLater(new Runnable() {
-      def run() = {
-        setSize(250, 200);
-        setTitle("Points");
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true)
-        add(t);
-      }
-    });
+
+      SwingUtilities.invokeLater(new Runnable() {
+        def run() = {
+          setSize(250, 200);
+          setTitle("Points");
+          setLocationRelativeTo(null);
+          setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          setVisible(true)
+          add(t);
+        }
+      });
+
+    } else {
+      println(halp)
+      sys.exit(1)
+    }
   }
 }
